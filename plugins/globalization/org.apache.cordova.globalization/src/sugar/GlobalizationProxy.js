e6cce829f1c1fb6c093f95098033199dff9aa942
puneetgkaur <puneet.gkaur@gmail.com>
2014-08-24 01:41:28 +0530

    commit

---
+++ plugins/globalization/org.apache.cordova.globalization/src/sugar/GlobalizationProxy.js
@@ -0,0 +1,63 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+var bus = require('cordova/sugar/bus'),
+    argscheck = require('cordova/argscheck'),
+    GlobalizationError = require('org.apache.cordova.globalization.GlobalizationError');
+
+bus.listen();
+
+var globalization = {
+    getPreferredLanguage:function(successCB, failureCB) {
+	function onResponseReceived(err, result) {
+
+	    if (!err) {
+		console.log("result : "+JSON.stringify(result));
+		console.log("Its success");
+		successCB(result);
+	    } else {
+		console.log("error:"+JSON.stringify(err));
+		console.log("Its error");
+                failureCB(err);
+	    }
+	}
+ 	bus.sendMessage("activity.cordova_GlobalizationPlugin",['getPreferredLanguage'],onResponseReceived);
+    },
+    getLocaleName:function(successCB, failureCB) {
+
+	function onResponseReceived(err, result) {
+
+	    if (!err) {
+		console.log("result : "+JSON.stringify(result));
+		console.log("Its success");
+		successCB(result);
+	    } else {
+		console.log("error:"+JSON.stringify(err));
+		console.log("Its error");
+                failureCB(err);
+	    }
+	}
+ 	bus.sendMessage("activity.cordova_GlobalizationPlugin",['getLocaleName'],onResponseReceived);
+    }
+};
+
+module.exports = globalization;
+
+require("cordova/sugar/commandProxy").add("Globalization", module.exports);
---
+++ plugins/sample apps/sugar_cordova_accelerometer/plugins/org.apache.cordova.device-motion/src/sugar/accelerometer.js
@@ -0,0 +1,106 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+var bus = require('cordova/sugar/bus');
+
+
+bus.listen();
+
+setInterval(function(){
+		function onResponseReceived(err, result) {
+		    if (!err) {
+			//console.log("result : "+JSON.stringify(result));
+			//console.log("result : "+result);
+			//console.log("Its success");
+		        result.timestamp = new Date().getTime();			
+		        cordova.fireWindowEvent('devicemotion',result);
+		    } else {
+			//console.log("error:"+JSON.stringify(err));
+			//console.log("Its error");
+			//cordova.callbackError(callbackId,error);
+			console.log(err);
+		    }
+		}
+	 	bus.sendMessage("activity.cordova_AccelerometerPlugin",[],onResponseReceived);
+           },5000);
+
+
+var accelerometer_running=false;
+
+
+function listener(success, ev) {
+    //console.log("ev:"+JSON.stringify(ev));
+    
+    //acc.timestamp = new Date().getTime();
+    success(ev);
+}
+
+
+var Accelerometer = {
+    start: function start(success, error) {
+        console.log("In start acceleration");
+        console.log("accelerometer_running = "+accelerometer_running);
+
+        accelerometer_running=true;
+
+
+        console.log("accelerometer_running = "+accelerometer_running);
+/*
+
+	function onResponseReceived(err, result) {
+
+	    if (!err) {
+		//successhandler(result);
+		console.log("result : "+JSON.stringify(result));
+		//console.log("result : "+result);
+
+		console.log("Its success");
+		
+		success(result);
+
+	    } else {
+		console.log("error:"+JSON.stringify(error));
+		console.log("Its error");
+
+		error(err);
+	    }
+	}
+
+
+ 	bus.sendMessage("activity.accelerometer",[],onResponseReceived);
+*/
+
+        return window.addEventListener('devicemotion', function(ev) {
+            listener(success, ev);
+        }, false);
+    },
+
+    stop: function stop() {
+        console.log("In stop acceleration");
+
+        accelerometer_running=false;
+        console.log("accelerometer_running = "+accelerometer_running);
+
+        window.removeEventListener('devicemotion', listener, false);
+    }
+};
+
+module.exports = Accelerometer;
+require('cordova/sugar/commandProxy').add('Accelerometer', Accelerometer);
